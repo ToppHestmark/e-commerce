455eee22f79f0b00396371dad2932221a4db3634
Suphachai Hestmark <top_hestmark@yahoo.no>
2021-01-18 16:38:22 +0100

    Added cart layout

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -10,6 +10,7 @@ export default makeStyles((theme) => ({
       width: `calc(100% - ${drawerWidth}px)`,
       marginLeft: drawerWidth,
     },
+    position: "relative",
   },
   title: {
     flexGrow: 1,
